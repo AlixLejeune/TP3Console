bc300f190ab58f7e00c408cc9245ef3588780390
AlixLejeune <alix.lejeune@etu.univ-smb.fr>
2024-02-12 16:57:23 +0100

    Section 2.1 classes partielles

---
+++ TP3Console/Models/EntityFramework/Categorie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace TP3Console.Models.EntityFramework
+{
+    [Table("categorie")]
+    public partial class Categorie
+    {
+        private ILazyLoader _lazyLoader;
+        //private ICollection<Film> films;
+
+        public Categorie(ILazyLoader lazyLoader)
+        {
+            _lazyLoader = lazyLoader;
+        }
+        public Categorie()
+        {
+            Films = new HashSet<Film>();
+        }
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("nom")]
+        [StringLength(50)]
+        public string Nom { get; set; } = null!;
+        [Column("description")]
+        public string? Description { get; set; }
+
+        [InverseProperty(nameof(Film.CategorieNavigation))]
+        public virtual ICollection<Film> Films { get; set; }
+        /*public virtual ICollection<Film> Films
+        {
+            get
+            {
+                return _lazyLoader.Load(this, ref films);
+            }
+            set { films = value; }
+        }*/
+    }
+}
--- TP3Console/Program.cs
+++ TP3Console/Program.cs
@@ -1,10 +1,86 @@
-﻿namespace TP3Console
+﻿
+using System;
+using System.Linq;
+using TP3Console.Models.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace TP3Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*using( FilmsDBContext ctx = new FilmsDBContext())
+            {
+
+                //Désactivation du tacking => pas de modif de la base
+                //ctx.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+                Film titanic = ctx.Films.First(f => f.Nom.Contains("Titanic"));
+                titanic.Description = "un bateau échoué. Date : " + new DateTime(1912, 4, 15);
+
+                int nbchanges = ctx.SaveChanges();
+                Console.WriteLine("nombre d'enregistrement modif ou ajout : " + nbchanges);
+            }*/
+
+            //chargement manuel de la categorie, puis des films qui appartiennent à cette catégorie
+            /*using (var ctx = new FilmsDBContext())
+            {
+                //Chargement de la catégorie Action
+                Categorie categorieAction = ctx.Categories.First(c => c.Nom == "Action");
+                Console.WriteLine("Categorie : " + categorieAction.Nom);
+                Console.WriteLine("Films : ");
+                //Chargement des films de la catégorie Action.
+                foreach (var film in ctx.Films.Where(f => f.CategorieNavigation.Nom ==
+                categorieAction.Nom).ToList())
+                {
+                    Console.WriteLine(film.Nom);
+                }
+            }*/
+
+            //chargement explicite
+            using (var ctx = new FilmsDBContext())
+            {
+                Categorie categorieAction = ctx.Categories.First(c => c.Nom == "Action");
+                Console.WriteLine("Categorie : " + categorieAction.Nom);
+                //Chargement des films dans categorieAction
+                ctx.Entry(categorieAction).Collection(c => c.Films).Load();
+                Console.WriteLine("Films : ");
+                foreach (var film in categorieAction.Films)
+                {
+                    Console.WriteLine(film.Nom);
+                }
+            }
+
+            //chargement hâtif
+            /*using (var ctx = new FilmsDBContext())
+            {
+                //Chargement de la catégorie Action et des films de cette catégorie
+                Categorie categorieAction = ctx.Categories
+                .Include(c => c.Films)
+                .First(c => c.Nom == "Action");
+                Console.WriteLine("Categorie : " + categorieAction.Nom);
+                Console.WriteLine("Films : ");
+                foreach (var film in categorieAction.Films)
+                {
+                    Console.WriteLine(film.Nom);
+                }
+            }*/
+
+            //chargement delayed / lazy
+            /*using (var ctx = new FilmsDBContext())
+            {
+                //Chargement de la catégorie Action
+                Categorie categorieAction = ctx.Categories.First(c => c.Nom == "Action");
+                Console.WriteLine("Categorie : " + categorieAction.Nom);
+                Console.WriteLine("Films : ");
+                //Chargement des films de la catégorie Action.
+                foreach (var film in categorieAction.Films) // lazy loading initiated
+                {
+                    Console.WriteLine(film.Nom);
+                }
+            }*/
+            Console.ReadLine();
         }
     }
 }
